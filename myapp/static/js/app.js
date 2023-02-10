9f351ffe5069bf50755b54160b730a5faea50178
Kristina <kristinalitvak@gmail.com>
2023-02-10 01:46:51 -0600

    generator form

--- myapp/static/js/app.js
+++ myapp/static/js/app.js
@@ -1,6 +1,7 @@
 // destination field for generator form - using Google Maps Place Autocomplete API
 var destinationInput = document.getElementById("destination");
-var autocomplete = new google.maps.places.Autocomplete(destinationInput);
+var autocomplete = new google.maps.places.Autocomplete(destinationInput);    
+
 
 // scrolls to the generator form when user clicks "Get Started" button on home page
 function scrollToForm() {
