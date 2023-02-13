26d973932b04db87a56fdc109066d0e882f30481
Kristina <kristinalitvak@gmail.com>
2023-02-12 18:26:42 -0600

    generator-form-design

--- myapp/static/js/app.js
+++ myapp/static/js/app.js
@@ -1,6 +1,10 @@
 const form = document.querySelector('.generator-form');
 
 function initAutocomplete() {   // Callback function for the Google API
+    // destination field for generator form - using Google Maps Place Autocomplete API
+    var destinationInput = document.getElementById('destination');
+    var autocomplete = new google.maps.places.Autocomplete(destinationInput);   
+
     form.addEventListener('submit', (event) => {
         event.preventDefault();
     
@@ -32,15 +36,13 @@ function initAutocomplete() {   // Callback function for the Google API
         console.log('Check-in: ', checkin);
         console.log('Check-out: ', checkout);
     });
-    
-    // destination field for generator form - using Google Maps Place Autocomplete API
-    var destinationInput = document.getElementById('destination');
-    var autocomplete = new google.maps.places.Autocomplete(destinationInput);    
+
+    // var currentUnixTimestap = ~~(+new Date() / 1000);
 }
 
 // Scroll to the generator form when user clicks "Get Started" button on home page
 function scrollToForm() {
-    document.querySelector('.form-container').scrollIntoView({ 
+    document.querySelector('.bottom-container').scrollIntoView({ 
         behavior: 'smooth'
     });
 }
