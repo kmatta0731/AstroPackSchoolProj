7538f30af9e438c0c8f197e3a8ba744f2c2e19c9
Kristina <kristinalitvak@gmail.com>
2023-03-07 10:30:08 -0600

    activities form fix

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myapp9',
+        'NAME': 'myapp15',
         'USER': 'postgres',
         'PASSWORD': 'kriska105',
         'HOST': '0.0.0.0',
